3376ab6a75aa54e86d4084fa7f68c3e1681980e7
Loïc Touzard <loic.touzard@gmail.com>
2015-12-16 11:08:15 +0100

    Added separator style.

---
+++ app/src/main/java/com/insa/thibault/ihm/adapter/InvitationAdapter.java
@@ -0,0 +1,7 @@
+package com.insa.thibault.ihm.adapter;
+
+/**
+ * Created by loict on 16/12/2015.
+ */
+public class InvitationAdapter {
+}
---
+++ app/src/main/java/com/insa/thibault/ihm/view/fragment/OnInvitationSelectedListener.java
@@ -0,0 +1,7 @@
+package com.insa.thibault.ihm.view.fragment;
+
+/**
+ * Created by loict on 16/12/2015.
+ */
+public interface OnInvitationSelectedListener {
+}
