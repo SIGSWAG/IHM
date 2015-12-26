313d21482e25d139ace3296083643162ad072c82
Loïc Touzard <loic.touzard@gmail.com>
2015-12-26 09:49:42 +0100

    Compatibility, to avoid merge pourri

--- app/src/main/java/com/insa/thibault/ihm/adapter/InvitationAdapter.java
+++
@@ -1,7 +0,0 @@
-package com.insa.thibault.ihm.adapter;
-
-/**
- * Created by loict on 16/12/2015.
- */
-public class InvitationAdapter {
-}
--- app/src/main/java/com/insa/thibault/ihm/view/fragment/OnInvitationSelectedListener.java
+++
@@ -1,7 +0,0 @@
-package com.insa.thibault.ihm.view.fragment;
-
-/**
- * Created by loict on 16/12/2015.
- */
-public interface OnInvitationSelectedListener {
-}
